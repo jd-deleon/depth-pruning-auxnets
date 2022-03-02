2cbbfdd0f86aaea7aeb0ec31aab61f3b53ec0e93
jd-deleon <jddl68@gmail.com>
2022-03-02 16:17:40 +0800

    Add flattened kws model for hw deployment

--- mlperf-tiny/v0.5/training/keyword_spotting/keras_model.py
+++ mlperf-tiny/v0.5/training/keyword_spotting/keras_model.py
@@ -81,6 +81,68 @@ def get_model(args):
         tf.keras.layers.Dense(model_settings['label_count'], activation="softmax")
     ])
 
+  elif model_name == 'ds_cnn_flat':
+    print("DS CNN model invoked")
+    input_shape_flat = [model_settings['spectrogram_length']*model_settings['dct_coefficient_count']]
+    input_shape = [model_settings['spectrogram_length'], model_settings['dct_coefficient_count'],1]
+    filters = 64
+    weight_decay = 1e-4
+    regularizer = l2(weight_decay)
+    final_pool_size = (int(input_shape[0]/2), int(input_shape[1]/2))
+    
+    # Model layers
+    # Input pure conv2d
+    inputs = Input(shape=input_shape_flat)
+    x = tf.reshape(inputs, (-1, model_settings['spectrogram_length'],model_settings['dct_coefficient_count'],1))
+    # x = Reshape((model_settings['spectrogram_length'],model_settings['dct_coefficient_count'],1), input_shape=input_shape_flat)(inputs)
+    x = Conv2D(filters, (10,4), strides=(2,2), padding='same', kernel_regularizer=regularizer)(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    x = Dropout(rate=0.2)(x)
+
+    # First layer of separable depthwise conv2d
+    # Separable consists of depthwise conv2d followed by conv2d with 1x1 kernels
+    x = DepthwiseConv2D(depth_multiplier=1, kernel_size=(3,3), padding='same', kernel_regularizer=regularizer)(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    x = Conv2D(filters, (1,1), padding='same', kernel_regularizer=regularizer)(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+
+    # Second layer of separable depthwise conv2d
+    x = DepthwiseConv2D(depth_multiplier=1, kernel_size=(3,3), padding='same', kernel_regularizer=regularizer)(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    x = Conv2D(filters, (1,1), padding='same', kernel_regularizer=regularizer)(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+
+    # Third layer of separable depthwise conv2d
+    x = DepthwiseConv2D(depth_multiplier=1, kernel_size=(3,3), padding='same', kernel_regularizer=regularizer)(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    x = Conv2D(filters, (1,1), padding='same', kernel_regularizer=regularizer)(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+
+    # Fourth layer of separable depthwise conv2d
+    x = DepthwiseConv2D(depth_multiplier=1, kernel_size=(3,3), padding='same', kernel_regularizer=regularizer)(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+    x = Conv2D(filters, (1,1), padding='same', kernel_regularizer=regularizer)(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+
+    # Reduce size and apply final softmax
+    x = Dropout(rate=0.4)(x)
+
+    x = AveragePooling2D(pool_size=final_pool_size)(x)
+    x = Flatten()(x)
+    outputs = Dense(model_settings['label_count'], activation='softmax')(x)
+
+    # Instantiate model.
+    model = Model(inputs=inputs, outputs=outputs)
+
   elif model_name == 'ds_cnn':
     print("DS CNN model invoked")
     input_shape = [model_settings['spectrogram_length'], model_settings['dct_coefficient_count'],1]
